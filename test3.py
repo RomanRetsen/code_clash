6db6404a57f4aa0bbfddac175b4bc500d1af84d5
Roman Retsen <vatraltd@gmail.com>
2022-01-18 18:32:44 -0500

    small additions

--- test3.py
+++ test3.py
@@ -1,12 +1,19 @@
-import csv
-from random import randint
+def sample():
+    n = 0
+    def func():
+        print("n = ", n)
 
-headers = ['randint upto 10', 'randint ipto 100', 'randint upto 1000']
-rows = [{'randint upto 10' : randint(1,10), 'randint ipto 100':randint(1,100), 'randint upto 1000':randint(1,1000)}
-        for _ in range(10)]
-print(rows)
+    def setn(value):
+        nonlocal n
+        n = value
+    def getn():
+        return n
 
-with open("random.csv", "wt") as f:
-    f_csv = csv.DictWriter(f, headers)
-    f_csv.writeheader()
-    f_csv.writerows(rows)
+    func.setn = setn
+    func.getn = getn
+    return func
+
+s = sample()
+s.setn(4)
+s()
+print(s.getn())

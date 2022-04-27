45140d185a33f7192e85f931750751efb8da67bf
RomanIncognito <rshyn2@gmail.com>
2022-04-26 22:37:35 -0400

    small changes

--- test6.py
+++ test6.py
@@ -1,1 +1,13 @@
-import string
+class arithmetic():
+
+    @staticmethod
+    def take_square(num1):
+        return num1 * num1
+
+
+r = arithmetic.take_square(5)
+print(r)
+
+my_ar = arithmetic()
+r2 = my_ar.take_square(5)
+print(r2)

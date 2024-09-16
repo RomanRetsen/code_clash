d291e4a8fa926941e877da5467e173c856cf88d0
Roman Retsen <vatraltd@gmail.com>
2024-09-16 10:32:24 -0400

    test

--- a_temp9.py
+++ a_temp9.py
@@ -1,13 +1,22 @@
+import random
+
+
+
 # mean
-the_list2 = [10,23,10,19,21,23,21,17, 20.2]
+# the_list2 = [10,23,10,19,21,23,21,17, 20.2]
+#
+# the_list2_s = (sorted(the_list2))
+#
+# print(f"Sorted {the_list2_s}")
+# if len(the_list2_s) % 2 == 0:
+#     print(sum(the_list2_s[len(the_list2_s)//2-1:len(the_list2_s)//2+1]) // 2)
+# else:
+#     print(the_list2_s[len(the_list2_s)//2])
 
-the_list2_s = (sorted(the_list2))
+the_list = [random.randint(1, 100) for _  in range(20)]
 
-print(f"Sorted {the_list2_s}")
-if len(the_list2_s) % 2 == 0:
-    print(sum(the_list2_s[len(the_list2_s)//2-1:len(the_list2_s)//2+1]) // 2)
-else:
-    print(the_list2_s[len(the_list2_s)//2])
+print(the_list)
 
-
+the_list[2:4] = [1000,2000, 3000]
+print(the_list)
 
